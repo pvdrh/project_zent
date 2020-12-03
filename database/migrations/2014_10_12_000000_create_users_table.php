d17790b545fc3313f3e11594f0d20800810ed472
pvdrh <duyvpvnua@gmail.com>
2020-12-03 21:21:54 +0700

    sua seed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone');
+            $table->tinyInteger('role');
             $table->string('address')->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
